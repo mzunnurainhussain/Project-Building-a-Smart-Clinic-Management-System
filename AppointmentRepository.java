1abd944508e3249a0e14748ae04cbce7c6375752
mzunnurainhussain <50512951+mzunnurainhussain@users.noreply.github.com>
2025-08-13 12:11:03 +0500

    AppointmentRepository.java

--- AppointmentRepository.java
+++ AppointmentRepository.java
@@ -2,8 +2,12 @@ package com.project_back_end.repo;
 
 import com.project_back_end.models.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
     List<Appointment> findByPatientIdOrderByAppointmentTimeAsc(Long patientId);
+    List<Appointment> findByDoctorIdAndAppointmentTimeBetweenOrderByAppointmentTimeAsc(
+            Long doctorId, LocalDateTime start, LocalDateTime end);
 }
