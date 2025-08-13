9fd8b6c13a91c5cdab11ae70a0048a763f14a1a3
mzunnurainhussain <50512951+mzunnurainhussain@users.noreply.github.com>
2025-08-13 11:46:49 +0500

    DoctorService.java

--- DoctorService.java
+++ DoctorService.java
@@ -41,3 +41,16 @@ public class DoctorService {
         return repo.findBySpecialtyIgnoreCaseAndNameContainingIgnoreCase(specialty, name);
     }
 }
+
+
+
+package com.project_back_end.repo;
+
+import com.project_back_end.models.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    List<Doctor> findBySpecialtyIgnoreCase(String specialty);
+    List<Doctor> findBySpecialtyIgnoreCaseAndNameContainingIgnoreCase(String specialty, String name);
+}
