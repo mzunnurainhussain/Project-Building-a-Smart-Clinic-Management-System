85679b390a70cc2e3319392e3abded7a0df0bf5f
mzunnurainhussain <50512951+mzunnurainhussain@users.noreply.github.com>
2025-08-13 12:13:23 +0500

    PatientRepository.java

--- PatientRepository.java
+++ PatientRepository.java
@@ -3,5 +3,9 @@ package com.project_back_end.repo;
 import com.project_back_end.models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByEmail(String email);
+    Optional<Patient> findByEmailOrPhone(String email, String phone);
 }
